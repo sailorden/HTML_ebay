969083757d1bc87cb974a3374d0369a166843467
ycomieronperdices <carlosherce@ycomieronperdices.net>
2015-03-04 12:27:51 +0100

    log-2015-03-04

---
+++ application/logs/log-2015-03-04.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2015-03-04 12:08:08 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Base de datos desconocida 'html_ebay' C:\wamp\www\HTML_ebay\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2015-03-04 12:08:08 --> Unable to connect to the database
+ERROR - 2015-03-04 12:21:09 --> Severity: Warning --> Missing argument 1 for App_model::get_template(), called in C:\wamp\www\HTML_ebay\application\modules\app\controllers\app.php on line 36 and defined C:\wamp\www\HTML_ebay\application\modules\app\models\app_model.php 8
+ERROR - 2015-03-04 12:21:09 --> 404 Page Not Found: favicon.ico
+ERROR - 2015-03-04 12:21:10 --> 404 Page Not Found: favicon.ico
