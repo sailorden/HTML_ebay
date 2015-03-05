02f2413fc0992a3708a885805f16bcf54e94a6f2
ycomieronperdices <carlosherce@ycomieronperdices.net>
2015-03-05 14:04:27 +0100

    changes html in app

--- application/modules/app/controllers/app.php
+++ application/modules/app/controllers/app.php
@@ -18,6 +18,36 @@ class App  extends MX_Controller {
  	}
 	
 	public function template($id = 1){
+		
+		
+		/*if($this->App_model->get_temporal($id)){
+			redirect('app/html/'.$id.'/'.$this->App_model->get_temporal($id));
+		}else{
+			$data['html'] = $this->App_model->get_basic_html($id);
+		}*/
+
+		$data['title'] = "Panel de control | YCP Manager";
+			
+		$data['keywords'] = url_title($this->App_model->get_template($id), 'underscore');
+
+		$data['description'] = "";
+			
+		$data['reference'] = 'APP';
+
+		$data['view'] = url_title($this->App_model->get_template($id), 'underscore').'/'.url_title($this->App_model->get_template($id), 'underscore');
+			
+		$data['robots'] = 'noindex, nofollow';
+		
+		$data['js'] =  $this->load->view(url_title($this->App_model->get_template($id), 'underscore').'/js_module/js_module','',TRUE);
+		
+		$data['template'] = $this->App_model->get_template($id);
+		
+		$this->load->view('layout', $data);
+		
+		
+ 	}
+	
+	public function html($id = 1, $id_html){
 
 		$data['title'] = "Panel de control | YCP Manager";
 			
--- application/modules/app/models/app_model.php
+++ application/modules/app/models/app_model.php
@@ -5,9 +5,48 @@ class App_model extends CI_Model{
     	parent::__construct();
   	}
 	 
-   public function get_template($id_tempalte){
+   public function get_template($id_template){
    		
-		$this->db->where('id_template', $id_tempalte); 
+		$this->db->where('id_template', $id_template); 
+		$query = $this->db->get('templates');
+		
+		if ($query->num_rows() > 0){
+			
+			return $query->row()->name_template;
+		}
+		else{
+				
+			return FALSE;
+		}
+				
+	
+   }
+   
+   public function get_temporal($id_template){
+			
+   		$this->db->select('html.id_html');
+		$this->db->where('template.id_template', $id_template);
+		$this->db->where('html.state', 1);
+		$this->db->join('html', 'templates.id_template = html.id_template');  
+		$query = $this->db->get('templates');
+		
+		if ($query->num_rows() > 0){
+			
+			return $query->row()->name_template;
+		}
+		else{
+				
+			return FALSE;
+		}		
+	
+   }
+   
+   public function get_html($id_template, $id_html){
+			
+   		$this->db->select('html.name_html');
+		$this->db->where('template.id_template', $id_template);
+		$this->db->where('html.id_html', $id_html);
+		$this->db->join('html', 'templates.id_template = html.id_template');  
 		$query = $this->db->get('templates');
 		
 		if ($query->num_rows() > 0){
