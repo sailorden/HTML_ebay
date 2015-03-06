81b64758048adb4635517e6cbc4b6b6bdf6ca073
ycomieronperdices <bSn2KSeDSQns>
2015-03-06 13:32:35 +0100

    logs

--- application/logs/log-2015-03-06.php
+++ application/logs/log-2015-03-06.php
@@ -5,3 +5,53 @@ ERROR - 2015-03-06 11:47:32 --> 404 Page Not Found: css/mobile_blue/images/loadi
 ERROR - 2015-03-06 11:47:32 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
 ERROR - 2015-03-06 11:49:22 --> 404 Page Not Found: css/mobile_blue/images/loading.gif
 ERROR - 2015-03-06 11:49:23 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
+ERROR - 2015-03-06 11:03:48 --> 404 Page Not Found: css/mobile_blue/images/loading.gif
+ERROR - 2015-03-06 11:03:48 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
+ERROR - 2015-03-06 11:11:36 --> Severity: Parsing Error --> syntax error, unexpected '}' D:\wamp\www\HTML_ebay\application\modules\app\controllers\app.php 69
+ERROR - 2015-03-06 11:12:41 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 33 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 11:14:01 --> 404 Page Not Found: 
+ERROR - 2015-03-06 11:14:01 --> 404 Page Not Found: 
+ERROR - 2015-03-06 11:14:01 --> 404 Page Not Found: 
+ERROR - 2015-03-06 11:14:01 --> 404 Page Not Found: 
+ERROR - 2015-03-06 11:14:09 --> 404 Page Not Found: css/mobile_blue/images/loading.gif
+ERROR - 2015-03-06 11:14:09 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
+ERROR - 2015-03-06 11:17:29 --> 404 Page Not Found: css/mobile_blue/images/loading.gif
+ERROR - 2015-03-06 11:17:29 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
+ERROR - 2015-03-06 11:17:30 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 32 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 11:18:09 --> Severity: Error --> Call to undefined function show_exception() D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php 32
+ERROR - 2015-03-06 11:19:59 --> Severity: Warning --> Missing argument 1 for log_message(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 32 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 468
+ERROR - 2015-03-06 11:19:59 --> Severity: Warning --> Missing argument 2 for log_message(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 32 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 468
+ERROR - 2015-03-06 11:21:58 --> Severity: Warning --> Missing argument 1 for log_message(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 36 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 468
+ERROR - 2015-03-06 11:21:58 --> Severity: Warning --> Missing argument 2 for log_message(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 36 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 468
+ERROR - 2015-03-06 11:53:30 --> 404 Page Not Found: ../modules/app/controllers/App/h
+ERROR - 2015-03-06 11:53:32 --> 404 Page Not Found: ../modules/app/controllers/App/js
+ERROR - 2015-03-06 11:53:32 --> 404 Page Not Found: ../modules/app/controllers/App/css
+ERROR - 2015-03-06 11:53:32 --> 404 Page Not Found: ../modules/app/controllers/App/font-awesome-4.3.0
+ERROR - 2015-03-06 11:54:25 --> 404 Page Not Found: css/mobile_blue/images/loading.gif
+ERROR - 2015-03-06 11:54:25 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
+ERROR - 2015-03-06 11:54:28 --> 404 Page Not Found: ../modules/app/controllers/App/templa
+ERROR - 2015-03-06 11:54:29 --> 404 Page Not Found: ../modules/app/controllers/App/font-awesome-4.3.0
+ERROR - 2015-03-06 11:54:29 --> 404 Page Not Found: ../modules/app/controllers/App/css
+ERROR - 2015-03-06 11:54:29 --> 404 Page Not Found: ../modules/app/controllers/App/js
+ERROR - 2015-03-06 11:55:30 --> 404 Page Not Found: ../modules/app/controllers/App/templa
+ERROR - 2015-03-06 11:55:31 --> 404 Page Not Found: ../modules/app/controllers/App/font-awesome-4.3.0
+ERROR - 2015-03-06 11:55:31 --> 404 Page Not Found: ../modules/app/controllers/App/js
+ERROR - 2015-03-06 11:56:10 --> 404 Page Not Found: ../modules/app/controllers/App/templa
+ERROR - 2015-03-06 11:56:10 --> 404 Page Not Found: ../modules/app/controllers/App/font-awesome-4.3.0
+ERROR - 2015-03-06 11:56:14 --> 404 Page Not Found: css/mobile_blue/images/hint.gif
+ERROR - 2015-03-06 11:56:14 --> 404 Page Not Found: css/mobile_blue/images/loading.gif
+ERROR - 2015-03-06 11:56:14 --> Severity: Error --> Call to undefined function base_url() D:\wamp\www\HTML_ebay\application\views\errors\html\error_404.php 13
+ERROR - 2015-03-06 11:56:14 --> Severity: Error --> Call to undefined function base_url() D:\wamp\www\HTML_ebay\application\views\errors\html\error_404.php 13
+ERROR - 2015-03-06 12:09:06 --> Severity: Error --> Call to undefined function show_exception() D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php 48
+ERROR - 2015-03-06 12:10:15 --> Severity: Error --> Call to undefined function show_exception() D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php 48
+ERROR - 2015-03-06 12:18:50 --> Severity: Warning --> include(D:\wamp\www\HTML_ebay\application\views\errors\error_db.php): failed to open stream: No such file or directory D:\wamp\www\HTML_ebay\system\core\Exceptions.php 184
+ERROR - 2015-03-06 12:18:50 --> Severity: Warning --> include(): Failed opening 'D:\wamp\www\HTML_ebay\application\views\errors\error_db.php' for inclusion (include_path='.;C:\php\pear') D:\wamp\www\HTML_ebay\system\core\Exceptions.php 184
+ERROR - 2015-03-06 12:19:20 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 12:19:20 --> Severity: Warning --> include(D:\wamp\www\HTML_ebay\application\views\errors\error_db.php): failed to open stream: No such file or directory D:\wamp\www\HTML_ebay\system\core\Exceptions.php 184
+ERROR - 2015-03-06 12:19:20 --> Severity: Warning --> include(): Failed opening 'D:\wamp\www\HTML_ebay\application\views\errors\error_db.php' for inclusion (include_path='.;C:\php\pear') D:\wamp\www\HTML_ebay\system\core\Exceptions.php 184
+ERROR - 2015-03-06 12:21:34 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 12:21:35 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 12:21:35 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 12:21:35 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 12:23:45 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
+ERROR - 2015-03-06 12:23:45 --> Severity: Warning --> Missing argument 1 for show_error(), called in D:\wamp\www\HTML_ebay\application\modules\app\models\clone_template_model.php on line 48 and defined D:\wamp\www\HTML_ebay\system\core\Common.php 407
