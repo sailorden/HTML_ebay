6cadea0db47aa6b3dca9940bb13921799a32e164
ycomieronperdices <carlosherce@ycomieronperdices.net>
2015-03-06 14:07:05 +0100

    change style_html

--- application/modules/app/views/mobile_blue/mobile_blue.php
+++ application/modules/app/views/mobile_blue/mobile_blue.php
@@ -21,6 +21,54 @@
 		
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		
+		<?php if ($style): ?>
+			
+			<style>
+			
+			<?php if ($style->primary_color): ?>
+			
+			 	h3.menu_head, ul.nav li:hover, .resp-tab-item:hover, .resp-tab-active{
+					background: <?= $style->primary_color ?> !important;
+				}
+				
+				.shipping h3 {
+				  	background-color: <?= $style->primary_color ?> !important;
+				}
+				
+				span.actual {
+					color: <?= $style->primary_color ?> !important;
+				}
+			
+			<?php endif ?>
+			
+			<?php if ($style->secondary_color): ?>
+			 	
+			 	ul.nav, ul.women_pagenation li.active a, ul.women_pagenation li a:hover, .contact-form input[type="submit"]:hover, .acount-btn:hover, 
+			 		.login-right input[type="submit"]:hover, .register-but input[type="submit"]:hover{
+					background: <?= $style->secondary_color ?> !important;
+				}
+			
+			<?php endif ?>
+		
+			html,body{
+				
+				<?php if ($style->background_color): ?>
+				
+				 	background: <?= $style->background_color ?> !important;
+				
+				<?php endif ?>
+				
+				<?php if ($style->background_image): ?>
+				
+	   				background: url(../mobile_blue/images/<?=$style->background_image?>) repeat center top;
+				
+				<?php endif ?>
+			}
+			
+			</style>
+			
+		<?php endif ?>
+		
 		
 
 		<title><?= $title; ?></title>
@@ -29,6 +77,9 @@
 		
 
 	</head>
+	
+	
+	
 	
 	<body class="bg-html">
 		
