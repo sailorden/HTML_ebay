715e2e43176a3049f543419f0f33bd3aeff2c0eb
ycomieronperdices <carlosherce@ycomieronperdices.net>
2015-03-10 12:32:14 +0100

    add text input

--- application/modules/app/views/mobile_blue/include/description_template.php
+++ application/modules/app/views/mobile_blue/include/description_template.php
@@ -1,21 +1,22 @@
 <div class="cont1 span_2_of_a1">
-	<h1><?= $html->name_product ?></h1>
+	<div id=""><h1 id="text_name_product"><?= $html->name_product ?></h1><button id="name_product"  class="toggle_text buttons_toggle"><i class="fa fa-pencil"></i></button></div>
+	
 	
     <div class="price_single">
     	
     	<?php if ($html->old_price): ?>
 			
-			<span class="reducedfrom"><?= $html->old_price ?>€</span>
+			<span id="text_old_price" class="reducedfrom"><?= $html->old_price ?>€</span><button id="old_price"  class="toggle_text buttons_toggle"><i class="fa fa-pencil"></i></button>
 			
 		<?php endif ?>
 	  
-	  <span class="actual"><?= $html->price ?>€</span>
+	  <span id="text_price" class="actual"><?= $html->price ?>€</span><button id="price"  class="toggle_text buttons_toggle" ><i class="fa fa-pencil"></i></button>
 	</div>
 	
 	
 	<p id="dpro_description_product" class="dpro quick_desc"><?= $html->description_product ?></p>
 	
-    <div class="inner_toggle"><button id="description_product"  class="toggle"><i class="fa fa-pencil"></i></button></div>
+    <button id="description_product"  class="toggle buttons_toggle"><i class="fa fa-pencil"></i></button>
     
     <div class="row">
     	<div class="col-md-6">
--- js/ckeditor/inline.js
+++ js/ckeditor/inline.js
@@ -1,32 +1,60 @@
-$(document).ready(function () {
+$(document).ready(
 	
-	CKEDITOR.disableAutoInline = true;
-	var depro = [];
-	var isEditingEnabled = [];
-		    
-	 $('.toggle').click(function(){
-	 	
-	 	var id = $(this).attr('id');
-	 	var description = document.getElementById( 'dpro_'+id );
-	 	
-	 	if(isEditingEnabled[id]){
-	 		if (depro[id]){
-				depro[id].destroy();
-				depro[id] = null;
+	function () {
+	
+		CKEDITOR.disableAutoInline = true;
+		var depro = [];
+		var isEditingEnabled = [];
+			    
+		 $('.toggle').click(function(){
+		 	
+		 	var id = $(this).attr('id');
+		 	var description = document.getElementById( 'dpro_'+id );
+		 	
+		 	if(isEditingEnabled[id]){
+		 		if (depro[id]){
+					depro[id].destroy();
+					depro[id] = null;
+				}
+		 		description.setAttribute( 'contenteditable', false );
+				this.innerHTML = '<i class="fa fa-pencil"></i>';
+				isEditingEnabled[id] = false;
+		 	}else{
+		 		if ( !depro[id] ) {
+				 	depro[id] = CKEDITOR.inline( description, {
+						startupFocus: true
+					} );
+				}
+				description.setAttribute( 'contenteditable', true );
+				this.innerHTML = '<i class="fa fa-save"></i>';
+				isEditingEnabled[id] = true;
 			}
-	 		description.setAttribute( 'contenteditable', false );
-			this.innerHTML = '<i class="fa fa-pencil"></i>';
-			isEditingEnabled[id] = false;
-	 	}else{
-	 		if ( !depro[id] ) {
-			 	depro[id] = CKEDITOR.inline( description, {
-					startupFocus: true
-				} );
-			}
-			description.setAttribute( 'contenteditable', true );
-			this.innerHTML = '<i class="fa fa-save"></i>';
-			isEditingEnabled[id] = true;
-		}
-	 });
- });
+		 });
+ 	}
  
+ );
+ 
+ 
+ $(document).ready(function () {
+ 	
+ 		var isEditingEnabled = [];
+ 		
+ 		$('.toggle_text').click(function(){
+ 			
+ 			var id = $(this).attr('id');
+ 			var text = document.getElementById( 'text_'+id );
+ 			var inner = text.innerHTML;
+		
+ 			if(isEditingEnabled[id]){
+ 				this.innerHTML = '<i class="fa fa-pencil"></i>';
+	 			text.innerHTML = document.getElementById( 'input_'+id ).value; 
+	 			isEditingEnabled[id] = false;	
+ 			}else{
+ 				
+	 			this.innerHTML = '<i class="fa fa-save"></i>';
+	 			text.innerHTML = '<input type="text" class="input_toggle" id="input_'+id+'" value="'+ inner +'"/>'; 	
+	 			isEditingEnabled[id] = true;
+ 			}
+ 					
+ 		});
+ });
