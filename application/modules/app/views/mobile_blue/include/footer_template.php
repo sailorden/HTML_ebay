073bd00b6148033e3eef406b4b29daabf8cb1730
ycomieronperdices <carlosherce@ycomieronperdices.net>
2015-03-12 11:54:37 +0100

    change buttons style

--- application/modules/app/views/mobile_blue/include/footer_template.php
+++ application/modules/app/views/mobile_blue/include/footer_template.php
@@ -3,7 +3,7 @@
 	<div class="footer-grid">
 		<h3><?= $html -> title_footer_1 ?></h3>
 		<p id="dpro_text_footer_1" class="dpro quick_desc"><?= $html -> text_footer_1 ?></p>
-		<button id="text_footer_1" class="buttons_interface_textarea buttons_toggle"><i class="fa fa-pencil"></i></button>
+		<button id="text_footer_1" class="buttons_interface textarea"><i class="fa fa-pencil"></i></button>
 	</div>
 	
 <?php endif ?>
@@ -13,7 +13,7 @@
 	<div class="footer-grid">
 		<h3><?= $html -> title_footer_2 ?></h3>
 		<p id="dpro_text_footer_2" class="dpro quick_desc"><?= $html -> text_footer_2 ?></p>
-		<button id="text_footer_2" class="buttons_interface_textarea buttons_toggle"><i class="fa fa-pencil"></i></button>
+		<button id="text_footer_2" class="buttons_interface textarea"><i class="fa fa-pencil"></i></button>
 	</div>
 	
 	
