940e5b9399f26e46dd4097efb93a4a1e4cdcd2e1
ycomieronperdices <bSn2KSeDSQns>
2015-03-16 13:56:02 +0100

    delete head

--- js/mobile_blue/interface.js
+++ js/mobile_blue/interface.js
@@ -34,7 +34,7 @@ var ClickActionButton = {
         
         ButtonModalForm : function() {
         	
-<<<<<<< HEAD
+
             $('#modal_form').live('click',ClickActionButton.ModalForm);
            
         },
@@ -42,10 +42,9 @@ var ClickActionButton = {
         ButtonEditEach : function() {
         	
             $('.buttons_interface.each').click(ClickActionButton.EachItem);
-=======
             $('#modal_form').click(ClickActionButton.ModalForm);
             $('.modal_form').live('click',ClickActionButton.ModalForm);
->>>>>>> f6e2e8aaf09b95d186f4c1a491d887f193f5e147
+
            
         },
         
